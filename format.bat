96954e9d00603abe1a30d684f7cd2e9f3f81ba46
Alexey Kasyanchuk <degitx@gmail.com>
2025-10-04 15:21:57 +0300

    cleanup old structure, prepare fo 2.0

--- format.bat
+++
@@ -1,2 +0,0 @@
-"d:\Program Files\Git\bin\bash.exe" -c "find src tests -name '*.js' ! -type d -exec bash -c 'expand -t 4 \"$0\" > /tmp/e && mv /tmp/e \"$0\"' {} \;"
-node_modules\.bin\eslint --fix src tests
